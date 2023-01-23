a505c66f5ca21b873b0d7d71a3cd7a6e96926b45
HIMATH-813 <94510456+HIMATH-813@users.noreply.github.com>
2023-01-23 08:45:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,26 +2,26 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.botname = "Queen Anya" 
+global.botname = "☬ 𝐐𝐮𝐞𝐞𝐧 𝐀𝐧𝐲𝐚 ☬" 
 global.botLanguage = "default" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || 𝗔𝗿𝗮𝗯𝗶𝗰 \\
-global.author = "𝙋𝙄𝙆𝘼"  
+global.author = "𝙷𝙸𝙼𝙰𝚃𝙷"  
 global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
-global.myweb = "https://github.com/PikaBotz" // You can put any link 
-global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
-global.headerText = "Anya by Pika" // text for header of menu         
-global.DocumentTitle = "MASTER⚔️PIKA" 
+global.myweb = "https://github.com/HIMATH-813" // You can put any link 
+global.waterMark = "×͜× King ✘ mr.Himath™ ✓" // Your own watermark 
+global.headerText = "Anya by HIMATH" // text for header of menu         
+global.DocumentTitle = "MASTER⚔️HIMA" 
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['916900904828'] 
-global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
+global.ownername = "𝐇𝐢𝐦𝐚𝐭𝐡 𝐍𝐞𝐭𝐡𝐯𝐢𝐝𝐮"   // Owner name //        
+global.ownernumber = ['94712923200'] 
+global.socialm = "𝗜𝗻𝘀𝘁𝗮 : _Himath__Nethvidu___" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
 global.ytname = "YT: Ytname"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
-global.region = "India" // your country name
-global.state = "Assam" // your state name
-global.district = "Dibrugarh" // your district name
+global.region = "Srilanka" // your country name
+global.state = "Southern" // your state name
+global.district = "Matara" // your district name
 global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "3.69_pika" // Your insta ID
+global.instagramId = "_Himath__Nethvidu___" // Your insta ID
 global.email = "example@exmaple.com" // put your email here
 
 
